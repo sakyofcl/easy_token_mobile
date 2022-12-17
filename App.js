7232247ffed40cc67b4a1089ee6c5a78439658c0
saky <sakyofcl@gmail.com>
2022-12-17 20:56:14 +0530

    inital commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView,View,Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>mini</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
